d15e5c0982145428a744d96678e2a7942d0b386d
arielbk <buchwald.kearney@gmail.com>
2018-06-18 18:32:06 +0200

    create-react-app init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <img alt="Lucid infinity logo" className="logo" src={require('./images/lucid.png')} />
+      </div>
+    );
+  }
+}
+
+export default App;
