f238c4f140f6e130189455bb855e87cb67847c68
arielbk <buchwald.kearney@gmail.com>
2018-07-29 18:55:59 +0200

    task select animation

--- src/App.js
+++ src/App.js
@@ -13,7 +13,9 @@ class AddProject extends Component {
 
 // to split into sub-components...
 class TaskList extends Component {
-  render() { return (
+  
+  render() { 
+    return (
     <div>
     <div className='project-group'>
         <input type="checkbox" className='toggle-collapse' name='toggle-collapse' />
@@ -25,23 +27,27 @@ class TaskList extends Component {
         </h3>
 
       <ul className='project-tasks'>
-        <li className='project-task'>
+        <li>
           <input type="checkbox" /> <span className="checkTask" />
           <span className="task-item">
             Define our MVP for the project
           </span>
+          <div className='task-border' />
         </li>
-        <li className='project-task project-task-selected'>
+        <li>
           <input type="checkbox" /> <span className="checkTask" />
           <span className="task-item">
             Clear direction for design and workflow
           </span>
+          <div className='task-border' />
         </li>
-        <li>
+        <li onClick={e => [...e.target.parentElement.children].filter(el=> el.classList.contains('task-border'))[0].classList.add('mwidth-100')}>
         <input type="checkbox" /> <span className="checkTask" />
           <span className="task-item">
             Define the components we will pull together
           </span>
+          {/* Here would go 'if project task is selected then add in this div' */}
+          <div className='task-border' />
         </li>
       </ul>
   </div>
@@ -56,17 +62,19 @@ class TaskList extends Component {
         </h3>
 
       <ul className='project-tasks'>
-        <li className='project-task'>
+        <li>
           <input type="checkbox" /> <span className="checkTask" />
           <span className="task-item">
             Get audio working in React
           </span>
+          <div className='task-border' />
         </li>
         <li>
         <input type="checkbox" /> <span className="checkTask" />
           <span className="task-item">
             Implement settings for global and each individual timer
           </span>
+          <div className='task-border' />
         </li>
       </ul>
     </div>
