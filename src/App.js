e8b0c9a02e94debb777868619a861e190c675c74
arielbk <buchwald.kearney@gmail.com>
2018-08-01 10:35:20 +0200

    basic implementation of sidebar tools system

--- src/App.js
+++ src/App.js
@@ -48,12 +48,19 @@ class TaskList extends Component {
 }
 
 class Sidebar extends Component {
-  render() { return (
+  constructor(props) {
+    super(props);
+    this.uuidv4 = require('uuid/v4');
+  }
+  render() { 
+    return (
     <div className="sidebar">
-      <h2 className='sidebar-main-title'>Clear direction for design and workflow</h2>
-      <Pomodoro />
+      <h2 className='sidebar-main-title'>{this.props.selectedTask ? this.props.selectedTask.name : 'Select a task'}</h2>
+      {this.props.selectedTask && this.props.selectedTask.tools && this.props.selectedTask.tools.map(tool => <Pomodoro key={this.uuidv4()}/>)}
       {/* And any other tools for this timer... */}
-      <h3 className='add-tool'>Add Tool</h3>
+      {this.props.selectedTask &&
+        (<h3 onClick={() => this.props.onAddTool(this.props.selectedTask.id)} className='add-tool'>+ Add Tool</h3>)
+      }
 
     </div>
   )}
@@ -82,43 +89,57 @@ class App extends Component {
           id: uuidv4(),
           project: this.chinguId,
           selected: false,
+          tools: [],
         },
         {
           name: 'Clear direction for design and workflow',
           id: uuidv4(),
           project: this.chinguId,
           selected: false,
+          tools: [],
         },
         {
           name: 'Define the components we will pull together',
           id: uuidv4(),
           project: this.chinguId,
           selected: false,
+          tools: [],
         },
         {
           name: 'Get audio working in React',
           id: uuidv4(),
           project: this.anotherId,
           selected: false,
+          tools: [],
         },
         {
           name: 'Implement settings for global and each individual timer',
           id: uuidv4(),
           project: this.anotherId,
           selected: false,
+          tools: [],
         },
       ]
     }
     this.handleSelectTask = this.handleSelectTask.bind(this);
+    this.handleAddTool = this.handleAddTool.bind(this);
   }
 
   handleSelectTask(e) {
-    console.log(e.target.dataset.id);
     const tasks = [...this.state.tasks];
     tasks.forEach(task => {
       task.id === e.target.dataset.id
         ? task.selected = !task.selected
         : task.selected = false;
+    });
+    this.setState({ tasks });
+  }
+
+  // for now it can just add a pomdoro component
+  handleAddTool(taskID) {
+    const tasks = [...this.state.tasks];
+    tasks.forEach(task => {
+      if (task.id === taskID) task.tools.push('Pomodoro');
     });
     this.setState({ tasks });
   }
@@ -141,7 +162,8 @@ class App extends Component {
 
           </div>
 
-          <Sidebar />
+          {/* sidebar get passed whichever task is selected as a prop */}
+          <Sidebar selectedTask={this.state.tasks.filter(task => task.selected)[0]} onAddTool={this.handleAddTool}/>
 
         </div>
         <div className="footer"></div>
