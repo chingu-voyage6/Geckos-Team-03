4b4832d908b2fcc2643b66ba4df0e81ba00ec3dc
arielbk <buchwald.kearney@gmail.com>
2018-08-06 13:42:14 +0200

    task input, header and background style changes

--- src/App.js
+++ src/App.js
@@ -217,16 +217,22 @@ class Sidebar extends Component {
     super(props);
     this.uuidv4 = require('uuid/v4');
   }
+
+  componentWillUnmount() {
+    console.log('sidebar-gone');
+  }
+
   render() { 
     return (
     <div className="sidebar">
-      {!this.props.selectedTask && <h3 className='side-info'>Click a task to reveal tools</h3>}
-      {this.props.selectedTask && this.props.selectedTask.tools && this.props.selectedTask.tools.map(tool => <Pomodoro key={tool.id} thisTool={tool} onDeleteTool={this.props.onDeleteTool} />)}
-      {/* And any other tools for this timer... */}
-      {this.props.selectedTask &&
-        (<div onClick={() => this.props.onAddTool(this.props.selectedTask.id)} className='btn-add-tool'>Add Tool</div>)
-      }
-
+      <div ref="sidebarContent" className="sidebar-content">
+        {!this.props.selectedTask && <h3 className='side-info'>Click a task to reveal tools</h3>}
+        {this.props.selectedTask && this.props.selectedTask.tools && this.props.selectedTask.tools.map(tool => <Pomodoro key={tool.id} thisTool={tool} onDeleteTool={this.props.onDeleteTool} />)}
+        {/* And any other tools for this timer... */}
+        {this.props.selectedTask &&
+          (<div onClick={() => this.props.onAddTool(this.props.selectedTask.id)} className='btn-add-tool'>Add Tool</div>)
+        }
+      </div>
     </div>
   )}
 }
